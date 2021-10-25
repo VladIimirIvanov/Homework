10c8c4dac3d1734abe63cfff029de92aaaba05e4
Vladimir Ivanov <vlad.ivanov.qa@list.ru>
2021-10-25 16:18:51 +0300

    modifed

--- src/lesson2/HW2.java
+++ src/lesson2/HW2.java
@@ -1,2 +1,51 @@
-package lesson2;public class HW2 {
+package lesson2;
+
+public class HW2 {
+
+    public static void main (String[] args) {
+
+        from10to20(10,10);
+        positiveOrNegative(0);
+        negativeCheckout(0);
+        printText("Слава роботам!",5 );
+    }
+
+    public static boolean from10to20(int a, int b) {
+
+        int result = a + b;
+
+        if (result >= 10 && result <=20) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static void positiveOrNegative(int a) {
+
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        }
+        else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    public static boolean negativeCheckout(int a) {
+
+        if (a < 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static void printText(String text, int a) {
+
+        for (int i = 0; i < a; i++) {
+            System.out.println(text);
+        }
+    }
 }
